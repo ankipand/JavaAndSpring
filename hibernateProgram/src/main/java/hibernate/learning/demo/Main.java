ce5fd591e9c86d872db250ef6fad944807f894d1
8440Gau <8440gaurav@gmail.com>
2020-02-03 21:29:30 +0530

    Updating

---
+++ hibernateProgram/src/main/java/hibernate/learning/demo/Main.java
@@ -0,0 +1,15 @@
+package hibernate.learning.demo;
+
+import java.util.logging.Logger;
+
+public class Main {
+private static final Logger LOGGER = Logger.getLogger("Hibernate-Tutorial");
+
+public static void main(String[] args) {
+	Main main = new Main();
+	
+}
+public void run() {
+	//SessionFactory 
+}
+}
